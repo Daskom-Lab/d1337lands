9681f26460e0ab76fc090a713c71cf0acfb206c4
fakhrip <fakhrip@protonmail.com>
2022-04-05 20:25:09 +0700

    feature: create initial layout structure

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ], 
+  theme: {
+    fontFamily: {
+      merriw: ['"Merriweather Sans"'],
+      overpassm: ['"Overpass Mono"']
+    }
+  },
+  plugins: [],
+}
