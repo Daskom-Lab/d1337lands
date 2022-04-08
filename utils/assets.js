9b8cbe3f93eb7deed1d712c6e06b726c4eadc0df
fakhrip <fakhrip@protonmail.com>
2022-04-08 11:26:26 +0700

    feature: add root dir listing to asssets

--- utils/assets.js
+++ utils/assets.js
@@ -13,7 +13,9 @@ export function getAssetFileAbs(filepath) {
 }
 
 export function getFileTree (dir=filetreeDir, files_){
-  files_ = files_ || {};
+  files_ = files_ || {
+    "/": []
+  };
   var files = fs.readdirSync(dir);
 
   for (var i in files){
@@ -21,15 +23,15 @@ export function getFileTree (dir=filetreeDir, files_){
     let filetreeName = name.replace(filetreeDir + "/", "")
     
     if (fs.statSync(name).isDirectory()){
-      if (filetreeName.includes("/")) {
-        const lastDir = filetreeName.split("/").slice(0, -1).join("/")
-        const currDir = filetreeName.split("/").pop() 
+      let lastDir = "/"
+      if (filetreeName.includes("/"))
+        lastDir = filetreeName.split("/").slice(0, -1).join("/");
+      const currDir = filetreeName.split("/").pop() 
         
-        files_[lastDir].push({
-          name: currDir,
-          type: "directory" 
-        })
-      }
+      files_[lastDir].push({
+        name: currDir,
+        type: "directory" 
+      })
             
       files_[filetreeName] = []
       getFileTree(name, files_);
