61321bbc75d6b57e6b35b89cce5c4b78138989f8
fakhrip <fakhrip@protonmail.com>
2022-04-22 08:29:46 +0700

    feature: add fontawesome and tweak some ui

--- webservice/pages/_app.js
+++ webservice/pages/_app.js
@@ -1,6 +1,11 @@
 import { AppWrapper } from '@/context/appstate'
 import '@/styles/globals.css'
 
+// Font awesome special config for next.js
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 function MyApp({ Component, pageProps }) {
   return (
     <AppWrapper>
--- webservice/pages/index.js
+++ webservice/pages/index.js
@@ -8,6 +8,8 @@ import { getAssetFile, getFileTree } from "@/utils/assets";
 import { getPeopleList } from '@/assets/peoples'
 import SimpleBar from 'simplebar-react';
 import 'simplebar/dist/simplebar.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGear } from '@fortawesome/free-solid-svg-icons'
 
 import dynamic from "next/dynamic";
 import { useAppContext } from '@/context/appstate'
@@ -57,19 +59,19 @@ export default function Home({ peopleList, fileTree }) {
         <link rel="icon" type="image/png" sizes="16x16" href="https://contact.daskomlab.com/assets/favicon/favicon-16x16.png" />
       </Head>
       <main className={
-        "w-full h-full border-8 border-green-400 bg-green-800 flex flex-row transition-all " +
+        "w-full h-full border-8 border-green-400 bg-green-800 flex flex-row transition-all duration-700 " +
         (sharedState.isGameActive ? "p-4" : "p-0")
       }>
         {
           sharedState.isGameActive && (
-            <div className="flex flex-col m-auto w-2/5 max-w-xl">
+            <div className="flex flex-col m-auto rounded-xl w-2/5 border-2 border-slate-400 bg-slate-900 mr-2 h-full max-w-xl">
               GAME LOG
             </div>
           )  
         }
         <div className={
-          "flex flex-col m-auto flex-auto transition-all " + 
-          (sharedState.isGameActive ? "h-full max-w-full" : "h-4/6 max-w-6xl")
+          "flex flex-col m-auto flex-auto transition-all duration-700 " + 
+          (sharedState.isGameActive ? "h-full max-w-full" : "h-[75%] max-w-6xl")
         }>
           <div className={
             "font-merriw font-extrabold text-4xl text-yellow-300 mx-auto " +
@@ -78,8 +80,8 @@ export default function Home({ peopleList, fileTree }) {
             Daskom1337 Community
           </div>
           <div className={
-            "w-full border-2 border-slate-400 bg-slate-900 rounded-xl shadow-l transition-all " + 
-            (sharedState.isGameActive ? "h-4/6" : "mt-6 h-full")
+            "w-full border-2 border-slate-400 bg-slate-900 rounded-xl shadow-l transition-all duration-700 " + 
+            (sharedState.isGameActive ? "h-4/6" : "mt-6 h-[75%]")
           }>
             {
               !sharedState.isGameActive ? (
@@ -94,11 +96,17 @@ export default function Home({ peopleList, fileTree }) {
                     }
                   </SimpleBar>
                 ) : (
-                  <Playground fileTree={fileTree}/>
+                  <Playground fileTree={fileTree} />
                 )
               ) : (
-                <div>
-                  GAME & Main Menu (Overlay)
+                <div className="w-full h-full text-white relative">
+                  <iframe src="http://localhost:8080" className="rounded-xl h-full w-full" />
+                  <button className="">
+                    <FontAwesomeIcon icon={faGear} />
+                  </button>
+                  <div className="w-full h-full top-0 rounded-xl absolute bg-slate-700 bg-opacity-80">
+
+                  </div>
                 </div>
               )
             }
@@ -112,8 +120,13 @@ export default function Home({ peopleList, fileTree }) {
                 <Button text="The Playground" color={menu === "playground" ? "red" : "yellow"} onClick={() => setMenu("playground")} />
               </div>
             ) : (
-              <div>
-                COMMENT & Sidebar (show people that is currently online)
+              <div className="flex flex-row mt-2 h-2/6">
+                <div className="flex flex-col mr-2 w-4/6 h-full rounded-lg border-2 border-slate-400 bg-slate-900">
+
+                </div>
+                <div className="flex flex-col w-2/6 h-full rounded-lg border-2 border-slate-400 bg-slate-900">
+
+                </div>
               </div>
             )
           }
