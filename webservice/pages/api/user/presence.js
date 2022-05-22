48523bc7c7e9a6b60a4b28d9cd6a0eaa7226a403
Cuday <66861117+abrahes12-day@users.noreply.github.com>
2022-05-22 21:11:48 +0700

    feature: add user presence api (#23)

---
+++ webservice/pages/api/user/presence.js
@@ -0,0 +1,33 @@
+import { postgresPool } from "@/utils/postgres";
+import { cors } from "@/utils/cors";
+import check_jwt from "@/utils/jwt";
+
+async function handler(req, res) {
+  await cors(req, res);
+
+  if (req.method !== "GET") {
+    res.status(400).send({ message: "Only GET requests allowed" });
+    return;
+  }
+
+  postgresPool.query(
+    `SELECT user_datas.user_id, user_datas.position, user_datas.map, users.username, users.role
+      FROM users
+      LEFT JOIN user_datas
+      ON user_datas.user_id = users.id`, 
+    (err, result) => {
+      if (err) {
+        res.status(500).json({
+          reason: "Data Not Found!"
+        });
+        return;
+      }
+
+      res.status(200).json({
+        result: result.rows,
+      });
+    }
+  );
+}
+
+export default check_jwt(handler)
