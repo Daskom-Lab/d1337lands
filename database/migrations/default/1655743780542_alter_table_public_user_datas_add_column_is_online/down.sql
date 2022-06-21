f21a4f7a6e905912ad32dfabf63b472fc0399026
fakhrip <fakhrip@protonmail.com>
2022-06-21 17:30:04 +0700

    feature: update permission for user_datas and add is_online column

---
+++ database/migrations/default/1655743780542_alter_table_public_user_datas_add_column_is_online/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."user_datas" add column "is_online" boolean
+--  not null default 'false';
---
+++ database/migrations/default/1655743780542_alter_table_public_user_datas_add_column_is_online/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."user_datas" add column "is_online" boolean
+ not null default 'false';
