8e7b371911f5b7ecf8da94d3844675582d960204
fakhrip <fakhrip@protonmail.com>
2022-06-28 11:22:05 +0700

    feature: add seeder for level and category table

---
+++ database/seeds/default/1656389869499_seed_initialize_level_data.sql
@@ -0,0 +1,10 @@
+SET check_function_bodies = false;
+INSERT INTO public.level (value, description) VALUES ('easyeasy', 'even babies can do it');
+INSERT INTO public.level (value, description) VALUES ('easymedium', 'babies probably cant, but toddlers definitely can');
+INSERT INTO public.level (value, description) VALUES ('easyhard', 'okay, its like elementary stuff');
+INSERT INTO public.level (value, description) VALUES ('mediumeasy', 'easy but not');
+INSERT INTO public.level (value, description) VALUES ('mediummedium', 'starts to get hot here');
+INSERT INTO public.level (value, description) VALUES ('mediumhard', 'its not hard but its definitely not easy');
+INSERT INTO public.level (value, description) VALUES ('hardeasy', 'its easy, but for the pros');
+INSERT INTO public.level (value, description) VALUES ('hardmedium', 'can college student do it though ?');
+INSERT INTO public.level (value, description) VALUES ('hardhard', 'lets freakin'' gooooooooooo');
---
+++ database/seeds/default/1656390105631_seed_initialize_category_data.sql
@@ -0,0 +1,8 @@
+SET check_function_bodies = false;
+INSERT INTO public.category (value, description) VALUES ('algoisland', 'you think binarysearch and bubblesort is the only way to go ?');
+INSERT INTO public.category (value, description) VALUES ('codeisland', 'hah! code ain''t easy ain''t it ?');
+INSERT INTO public.category (value, description) VALUES ('hackisland', 'hack all the things!');
+INSERT INTO public.category (value, description) VALUES ('dataisland', 'even the machine study really hard!');
+INSERT INTO public.category (value, description) VALUES ('netisland', 'networking != socialization');
+INSERT INTO public.category (value, description) VALUES ('town', 'where bunch of people stay in one place');
+INSERT INTO public.category (value, description) VALUES ('mentorcastle', 'if the lecturer have a special room, the mentor have a castle');
