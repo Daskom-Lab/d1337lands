449b66b6d7e4c50f7b10ab3a6ca3141eb136c696
fakhrip <fakhrip@protonmail.com>
2022-06-29 07:09:46 +0700

    feature: add submission list on quest api

---
+++ webservice/pages/api/quest/submission.js
@@ -0,0 +1,35 @@
+import { postgresPool } from "@/utils/postgres";
+import { cors } from "@/utils/cors";
+import check_jwt from "@/utils/jwt";
+
+async function handler(req, res) {
+  await cors(req, res);
+
+  if (req.method !== "GET") {
+    res.status(400).send({ message: "Only GET requests allowed" });
+    return;
+  }
+
+  postgresPool.query(
+    `SELECT submissions.answer, submissions.is_correct, 
+            quests.title, quests.category, quests.level
+      FROM submissions
+      LEFT JOIN quests
+      ON quests.id = submissions.quest_id
+      WHERE NOT submissions.is_redeemed`,
+    (err, result) => {
+      if (err) {
+        res.status(500).json({
+          reason: "Data Not Found!"
+        });
+        return;
+      }
+
+      res.status(200).json({
+        result: result.rows,
+      });
+    }
+  );
+}
+
+export default check_jwt(handler)
