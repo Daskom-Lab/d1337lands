d8af6f2cbe141f8d1e728d5d85e613d6d79947f7
unknown <abdillahnur09@gmail.com>
2022-10-31 12:38:29 +0700

    add abd ascii art & add abd to peoples.js

--- webservice/assets/peoples.js
+++ webservice/assets/peoples.js
@@ -135,6 +135,18 @@ export function getPeopleList() {
 |                      ~~~                       |
 |         2022 Academician at Daskom1337         |
 +------------------------------------------------+`,
+    },
+    {
+      Avatar: "abd.html",
+      Handle: "Bedilz",
+      Realname: "Abdillah Nur",
+      Description:`
++------------------------------------------------+
+|      they told me to fill the description      |
+|    with anything, welp i guess this is it      |
+|                      ~~~                       |
+|         2022 Academician at Daskom1337         |
++------------------------------------------------+` 
     }
   ];
 }
